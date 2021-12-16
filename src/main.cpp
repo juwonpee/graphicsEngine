34ae437cf84ac8cb6f499bd49056fb5abf72851b
root <juwonpee@gmail.com>
2021-12-16 19:50:57 +0900

    Fixed graphicsEngine bugs & made demo image

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include "types.h"
 #include "graphicsEngine.h"
+#include <thread>
+#include <string>
+#include <stdlib.h>
+
+using namespace GraphicsEngine;
+using namespace std;
+
+dataShare_t dataShare;
+
+void graphicsThread();
 
 int main() {
+    dataShare.lock.unlock();
+    thread* graphics = new thread(graphicsThread);
 
+    uint8_t count = 0;
+    while (1) {
+        count++;
+        pixel_t pixel;
+        pixel.r = count; pixel.g = count; pixel.b = count; pixel.a = count;
+
+        dataShare.lock.lock();
+        dataShare.pixels.push(pixel);
+        dataShare.lock.unlock();
+    }
+}
+
+void graphicsThread() {
+    graphicsEngine engine(800, 600, &dataShare, "Hello World!");
 }
