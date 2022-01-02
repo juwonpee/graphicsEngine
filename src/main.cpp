2d588060a60b5285d5140530fe944d35a61ca7af
root <juwonpee@gmail.com>
2022-01-02 13:58:33 +0900

    Minor performance optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,37 @@ int main() {
     dataShare.lock.unlock();
     thread* graphics = new thread(graphicsThread);
 
-    uint8_t count = 0;
+    uint8_t r, g, b, a = 0;
+    uint8_t state = 0;
     while (1) {
-        count++;
         pixel_t pixel;
-        pixel.r = count; pixel.g = count; pixel.b = count; pixel.a = count;
+        pixel.r = r; pixel.g = g; pixel.b = b; pixel.a = a;
 
-        if (count == 201) count = 0;
+        switch (state) {
+            case 0:
+                r++;
+                if (r > 254) {
+                    r = 0;
+                    state = 1;
+                }
+                break;
+            case 1:
+                g++;
+                if (g > 254) {
+                    g = 0;
+                    state = 2;
+                }
+                break;
+            case 2:
+                b++;
+                if (b > 254) {
+                    b = 0;
+                    state = 2;
+                }
+                break;
+        }
+        a++;
+        if (a >201) a = 0;
 
         dataShare.lock.lock();
         dataShare.pixels.push(pixel);
